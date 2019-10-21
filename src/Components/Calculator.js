9db3d0b70917a2b28395242efdb8e5f4843b9ddd
Szymon Trombik <szymon.trombik@hotmail.com>
2019-10-21 21:28:18 +0200

    added validation on clicked button to check if it is number or operation

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -7,18 +7,32 @@ class Calculator extends Component {
         super();
 
         this.state = {
-            result: "123"
+            result: "",
+
         }
     }
 
     onClick = button => {
-        alert(button)
+        if (isNaN(button)) {
+            switch(button)
+            {
+                case '+': alert('plus'); break;
+                case '-': alert('minus'); break;
+                case '*': alert('multi'); break;
+                case '/': alert('div'); break;
+                case '=': alert('sum'); break;
+            }
+        }
+        
+        this.setState({
+            result: this.state.result + button
+        })
     }
 
     render() {
         return <table>
             <Display result={this.state.result} />
-            <Keypad />
+            <Keypad onClick={this.onClick} />
         </table>;
     }
 }
